e57791dbdbc692a289f09bb080342c2a58bbf226
cacko <alex@cacko.net>
2023-11-22 18:54:43 +0000

    changes

--- src/app/components/jobstatus/jobstatus.component.ts
+++ src/app/components/jobstatus/jobstatus.component.ts
@@ -12,19 +12,26 @@ export class JobstatusComponent implements OnInit {
   @Input() truncate: boolean = false;
 
   icon: string = '';
+  label: string = '';
 
   ngOnInit(): void {
-      switch (this.status) {
-        case JobStatus.PENDING:
-          this.icon = "hourglass";
-          break;
-        case JobStatus.IN_PROGRESS:
-          this.icon = "sync";
-          break;
-        case JobStatus.REJECTED:
-          this.icon = "thumb_down";
-          break;
-      }
+
+    switch (this.status) {
+      case JobStatus.PENDING:
+        this.label = "Pending"
+        this.icon = "hourglass";
+        break;
+      case JobStatus.IN_PROGRESS:
+        this.label = "In Progres"
+        this.icon = "sync";
+        break;
+      case JobStatus.REJECTED:
+        this.label = "Rejected";
+        this.icon = "thumb_down";
+        break;
+      default:
+        this.label = this.status;
+    }
   }
 
 }
